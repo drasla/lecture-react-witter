2cb066d990756df5489b44344700fe19130b94e1
Tae.Garam <drasla@naver.com>
2025-12-11 15:22:12 +0900

    firebase init

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { RouterProvider } from "react-router";
 import router from "./router/router.tsx";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/LoadingScreen.tsx";
+import { auth } from "./firebase.ts";
 
 function App() {
     const [isLoading, setIsLoading] = useState(true);
 
     const init = async () => {
+        await auth.authStateReady();
         setIsLoading(false);
     };
 
